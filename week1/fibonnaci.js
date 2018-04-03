ff3a9f4357ca31ed4626af1dc1048f045e926906
igorlinux <igorlinux95@gmail.com>
2018-04-03 13:30:14 -0300

    Criando funcionalidade do Fibonnaci Memoizada

--- week1/fibonnaci.js
+++ week1/fibonnaci.js
@@ -1,20 +1,45 @@
 const addNumber = (a, b) => a + b;
 
-const fibonnaci = (index, arr = [1,1]) => {
+// A) Fibonnaci
+// const fibonnaci = (index, arr = [1,1]) => {
+//
+//     if (arr[index]) {
+//         return arr[index];
+//
+//     }
+//     else {
+//         arr.push(addNumber(arr[arr.length - 1], arr[arr.length - 2]));
+//         fibonnaci(index, arr);
+//     }
+// }
+
+// B) Memoizing Fibonnaci
+
+const fibonnaciByIndex = (index, arr = [1,1]) => {
 
     if (arr[index]) {
         return arr[index];
-
     }
     else {
         arr.push(addNumber(arr[arr.length - 1], arr[arr.length - 2]));
         fibonnaci(index, arr);
     }
-
 }
 
+const fibonnaciMemoized = () => {
+    let saved = {};
+    return (index) => {
+        if (index in saved) {
+            return saved[index];
+        }
+        else {
+            let result = fibonnaciByIndex(index);
+            saved[index] = result;
+            return result;
+        }
+    }
+}
+
+const fibonnaci = fibonnaciMemoized();
+
 module.exports = fibonnaci;
-
-
-
-
