1fd15ebeebab0938e5550de5f86e262f2a5ac18d
igorlinux <igorlinux95@gmail.com>
2018-04-03 16:48:26 -0300

    Começando a funcionalidade de Mais Repetidos

---
+++ week1/most_repeated.js
@@ -0,0 +1,50 @@
+var saveRepeated = (item, obj) => {
+
+    if (item in obj) {
+        obj[item]['repetition']++;
+    }
+    else {
+        obj[item] = {n: item, repetition: 1};
+    }
+    obj['mrepeat'] = compareRepetition(obj[item], obj['mrepeat']);
+
+    return obj;
+
+}
+
+var compareRepetition = (item1, item2) => {
+
+    var itemFinal;
+    if (item1['repetition'] > item2['repetition']) {
+        itemFinal = item1;
+    }
+    else if (item1['repetition'] == item2['repetition']) {
+        if (item1['n'] > item2['n']) {
+            itemFinal = item1;
+        }
+        else {
+            itemFinal = item2;
+        }
+    }
+    else {
+        itemFinal = item2;
+    }
+
+    return itemFinal;
+}
+
+var mostRepeated = (array, obj = {'mrepeat': {n: 0, repetition: 0}}) => {
+
+    var newArray = array.slice(1);
+
+    if (array.length > 0) {
+
+        var newObj = saveRepeated(array[0], obj);
+        mostRepeated(newArray, newObj);
+
+    }
+    else {
+        var result = obj['mrepeat']
+        return result;
+    }
+}
