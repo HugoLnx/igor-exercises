7981ad10434b0bc922bef75c1235f5649ccc4281
igorlinux <igorlinux95@gmail.com>
2018-04-03 10:42:14 -0300

    Funcionalidade Fibonnaci

--- week1/fibonnaci.js
+++ week1/fibonnaci.js
@@ -1,5 +1,20 @@
-function fibonnaci(number) {
-  return 0;
+const addNumber = (a, b) => a + b;
+
+const fibonnaci = (index, arr = [1,1]) => {
+
+    if (arr[index]) {
+        return arr[index];
+
+    }
+    else {
+        arr.push(addNumber(arr[arr.length - 1], arr[arr.length - 2]));
+        fibonnaci(index, arr);
+    }
+
 }
 
 module.exports = fibonnaci;
+
+
+
+
